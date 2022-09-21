a7f0137cd7b0d325106c8e495f271a61a1e2e3a1
jack-SAI <2449001989@qq.com>
2022-09-22 00:36:30 +0800

    增加help。=value 主软件上更新

--- ultrasound/ultrasound.ino
+++ ultrasound/ultrasound.ino
@@ -4,6 +4,7 @@
 #include <LiquidCrystal.h>   //引用1602液晶的函数库
 #include <string.h>
 // 设定SR04超声波模块连接的Arduino的数字口
+int workingdelay=100;//修改此变量可以修改检测数据上传的间隔
 const int EchoPin = 3; //Echo回声引脚连到数字好口3
 const int TrigPin = 2;  //Trig触发引脚连到数字口2
 int ledpin=13;//定义数字口13
@@ -27,17 +28,22 @@ void setup()
     pinMode(EchoPin, INPUT);    //超声模块Echo引脚产生的电平信号 输入到Arduino的EchoPin
    
     Serial.println("Ultrasonic sensor:");
-
 }
 void loop()
 {
   //操作逻辑：
   //@s.进入工作状态
   //@t.进入停止状态
-  //在工作状态下，属于pwm： 数字，进行pwm控制。
-    //注意冒号紧贴pwm，冒号和数字之间有空格
+  //在工作状态下，输入pwm 数字，进行pwm控制。
+    //pwm和数字之间有且只有一个空格。数字有效范围[0.255]
+    //即 pwm|空格|数字
     while(Serial.available()>0){ 
   //avilable判断，不能read。read()已经调用了
+
+  //2022年9月21日版本，尝试增加中断控制
+  //硬件中断，因为中断口IO2 IO3被占用。那部分代码是另一位同学所写
+  //总之硬件的外部中断没有达成。在串口的交互中做了一个类似中断的效果
+  //新增功能 help $para=value 可通过@s.后输入help 获取所有帮助
     cmmd+=char(Serial.read());
     delay(2);
     }
@@ -49,8 +55,14 @@ void loop()
       lcd.print("started");
     }else if(cmmd.startsWith("@t.")){
       mystatus=0;}
-      else if(mystatus==1 && cmmd.startsWith("pwm:")){
+      else if(mystatus==1 && cmmd.startsWith("pwm")){
         mystatus=2;}
+        else if(mystatus==1 && cmmd.startsWith("$")){
+          mystatus=3;}
+            else if(mystatus==1 && cmmd.startsWith("help")){
+              mystatus=4;}
+              else if(mystatus!=1 && mystatus!=0 && cmmd.startsWith("exit")){
+                mystatus=1;}
 
     switch(mystatus){
       case 0:
@@ -70,6 +82,16 @@ void loop()
         cmmd="";
         mystatus=1;
         break;
+      case 3:
+        interruptfunc();
+        cmmd="";
+        break;
+      case 4:
+        helpshow();
+        cmmd="";
+        break;
+      default:
+        Serial.println("Something Wrong!mystatus invalid");
     }
 
     
@@ -106,6 +128,8 @@ void working(){
     analogWrite(pwmledpin,pwmval);       
     //val+=5;//这里是调试的时候，判断pwm确实有效作用于灯泡了
     lcd.setCursor(1,3);
+    lcd.print("                ");//16空格，单独清空一行。
+    lcd.setCursor(1,3);           //光标回到需要的位置
     lcd.print("pwmval:");
     if(pwmval>=0&&pwmval<=255)
       lcd.print(pwmval); 
@@ -117,10 +141,54 @@ void working(){
     Serial.print("temperature: ");
     Serial.println(float_sensorValue,1);  //保留两位小数发送数据
 
-
+   delay(workingdelay);
   }
 
  void func_pwm(){
-    int colonplace=cmmd.indexOf(':');
-    pwmval=cmmd.substring(colonplace+2).toInt();
+    int charplace=cmmd.indexOf('m');
+    pwmval=cmmd.substring(charplace+2).toInt();
+
+    delay(400);
+  }
+
+  void interruptfunc(){
+    //if(!cmmd.equals("")){
+    int charplace1=cmmd.indexOf('$');
+    int charplace2=cmmd.indexOf('=');
+    String para=cmmd.substring(charplace1+1,charplace2);
+    //这里吃大亏，$+1下一个位置。开始没注意
+    if(para.compareTo("workingdelay")==0 || para.compareTo("wkdy")==0){
+      workingdelay=cmmd.substring(charplace2+1).toInt();
+      }
+    if(para.compareTo("pwmval")==0){
+      pwmval=cmmd.substring(charplace2+1).toInt();
+      }
+    if(para.compareTo("mystatus")==0){
+      mystatus=cmmd.substring(charplace2+1).toInt();
+      }
+    //}
+    }
+
+void helpshow(){
+  Serial.println("-----help------");
+  delay(1000);
+  Serial.println("use \"@s\". to start(this makes mystatus 1)");
+  delay(1000);
+  Serial.println("use \"@t\". to end(this makes mystatus 0)");
+  delay(1000);
+  Serial.println("use \"$para=value\"to assign(this makes mystatus 3)");
+  delay(1000);
+  Serial.println("valid para:pwmval,workingdelay(wkdy),mystatus");
+  delay(3000);
+  Serial.println("pwmval[0,255]");
+  delay(1000);
+  Serial.println("workingdelay(wkdy)[0,2**32-1],ms;");
+  delay(1000);
+  Serial.println("\teach time  the time delay after collecting");
+  delay(1000);
+  Serial.println("mystatus{0,1,2,3,4}");
+  delay(1000);
+  Serial.println("use exit to reboot to mystatus 1");
+  delay(5000);
+  mystatus=1;
   }
